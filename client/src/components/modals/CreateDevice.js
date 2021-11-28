7e4aa50c402a130115a2a66f309a347da67e7f41
Alexander Dashko <leksdashko@gmail.com>
2021-11-28 18:42:36 +0200

    pagination

--- client/src/components/modals/CreateDevice.js
+++ client/src/components/modals/CreateDevice.js
@@ -1,9 +1,14 @@
-import React, {useContext, useState} from 'react';
+import { observer } from 'mobx-react-lite';
+import React, {useContext, useState, useEffect} from 'react';
 import { Form, Modal, Button, Dropdown, Row, Col } from 'react-bootstrap';
 import { Context } from '../..';
+import { createDevice, fetchBrands, fetchTypes } from '../../http/deviceAPI';
 
 const CreateDevice = ({show, onHide}) => {
     const {device} = useContext(Context)
+    const [name, setName] = useState('')
+    const [price, setPrice] = useState(0)
+    const [file, setFile] = useState(null)
     const [info, setInfo] = useState([])
 
     const addInfo = () => {
@@ -13,6 +18,30 @@ const CreateDevice = ({show, onHide}) => {
     const removeInfo = (number) => {
         setInfo(info.filter(i => i.number !== number))
     }
+
+    const selectFile = e => {
+        setFile(e.target.files[0]);
+    }
+
+    const changeInfo = (key, value, number) => {
+        setInfo(info.map(i => i.number === number ? {...i, [key]: value} : i))
+    }
+
+    const addDevice = () => {
+        const formData = new FormData()
+        formData.append('name', name)
+        formData.append('price', `${price}`)
+        formData.append('img', file)
+        formData.append('brandId', device.selectedType.id)
+        formData.append('typeId', device.selectedBrand.id)
+        formData.append('info', JSON.stringify(info)) 
+        createDevice(formData).then(data => onHide())       
+    }
+
+    useEffect(() => {
+        fetchTypes().then(data => device.setTypes(data))
+        fetchBrands().then(data => device.setBrands(data))
+    }, [])
 
     return (
         <Modal
@@ -29,36 +58,40 @@ const CreateDevice = ({show, onHide}) => {
             <Modal.Body>
                 <Form>
                     <Dropdown>
-                        <Dropdown.Toggle>Choose type</Dropdown.Toggle>
+                        <Dropdown.Toggle>{device.selectedType.name || 'Choose type'}</Dropdown.Toggle>
                         <Dropdown.Menu>
                             {device.types.map(type =>
-                                <Dropdown.Item key={type.id}>{type.name}</Dropdown.Item>    
+                                <Dropdown.Item onClick={() => device.setSelectedType(type)} key={type.id}>{type.name}</Dropdown.Item>    
                             )}
                         </Dropdown.Menu>
                     </Dropdown>
                     <Dropdown>
-                        <Dropdown.Toggle>Choose brand</Dropdown.Toggle>
+                        <Dropdown.Toggle>{device.selectedBrand.name || 'Choose brand'}</Dropdown.Toggle>
                         <Dropdown.Menu>
                             {device.brands.map(brand =>
-                                <Dropdown.Item key={brand.id}>{brand.name}</Dropdown.Item>    
+                                <Dropdown.Item onClick={() => device.setSelectedBrand(brand)} key={brand.id}>{brand.name}</Dropdown.Item>    
                             )}
                         </Dropdown.Menu>
                     </Dropdown>
-                    <Form.Control className="mt-3" placeholder="Type device name"/>
-                    <Form.Control type="number" className="mt-3" placeholder="Type device price"/>
+                    <Form.Control value={name} onChange={e => setName(e.target.value)} className="mt-3" placeholder="Type device name"/>
+                    <Form.Control value={price} onChange={e => setPrice(Number(e.target.value))} type="number" className="mt-3" placeholder="Type device price"/>
                     <hr/>
-                    <Form.Control type="file" className="mt-3"/>
+                    <Form.Control type="file" onChange={selectFile} className="mt-3"/>
                     <hr/>
                     <Button variant={"outline-dark"} onClick={addInfo}>Add new device</Button>
                     {info.map(i => 
                         <Row key={i.number} className="mt-3">
                             <Col md={4}>
                                 <Form.Control 
+                                    value={i.title}
+                                    onChange={(e) => changeInfo('title', e.target.value, i.number)}
                                     placeholder="Type property title"
                                 />
                             </Col>
                             <Col md={4}>
                                 <Form.Control 
+                                    value={i.description}
+                                    onChange={(e) => changeInfo('description', e.target.value, i.number)}
                                     placeholder="Type property description"
                                 />
                             </Col>
@@ -71,10 +104,10 @@ const CreateDevice = ({show, onHide}) => {
             </Modal.Body>
             <Modal.Footer>
                 <Button variant="outline-danger" onClick={onHide}>Close</Button>
-                <Button variant="outline-success" onClick={onHide}>Add</Button>
+                <Button variant="outline-success" onClick={addDevice}>Add</Button>
             </Modal.Footer>
         </Modal>
     );
 };
 
-export default CreateDevice;
+export default observer(CreateDevice);
--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -7,7 +7,8 @@ const { type } = require('os')
 class DeviceController {
     async create(req, res, next) {
         try{
-            const {name,price,brandId, typeId, info} = req.body
+            const {name,price,brandId,typeId} = req.body
+            let { info } = req.body
             const {img} = req.files
             let fileName = uuid.v4() + ".jpg"
             img.mv(path.resolve(__dirname, '..', 'static', fileName))
